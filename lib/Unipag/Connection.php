ab86d78491d58ade6088423bd025bbc8d9957837
Denis Stebunov <denis.stebunov@ivelum.com>
2013-09-03 15:45:56 +0400

    add .get() and .reload() methods for Connection objects

--- lib/Unipag/Connection.php
+++ lib/Unipag/Connection.php
@@ -7,4 +7,15 @@ class Unipag_Connection extends Unipag_Object
         $class = get_class();
         return self::execFilter($class, $filter, $api_key);
     }
+
+    public static function get($id, $api_key=null)
+    {
+        $class = get_class();
+        return self::execGet($class, $id, $api_key);
+    }
+
+    public function reload()
+    {
+        return $this->execReload();
+    }
 }
