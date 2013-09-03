18f84820176c4e3ac7379632caab7b12eee018a6
Denis Stebunov <denis.stebunov@ivelum.com>
2013-09-03 15:46:41 +0400

    add .filter() method for Event objects

--- lib/Unipag/Event.php
+++ lib/Unipag/Event.php
@@ -2,6 +2,12 @@
 
 class Unipag_Event extends Unipag_Object
 {
+    public static function filter($filter=array(), $api_key=null)
+    {
+        $class = get_class();
+        return self::execFilter($class, $filter, $api_key);
+    }
+
     public static function get($id, $api_key=null)
     {
         $class = get_class();
