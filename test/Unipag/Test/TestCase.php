13a4f8c318f4e887f453d2b7d11281cc9bc32d09
Denis Stebunov <denis.stebunov@ivelum.com>
2013-09-03 15:54:41 +0400

    trying to fix broken build for 5.2

--- test/Unipag/Test/TestCase.php
+++ test/Unipag/Test/TestCase.php
@@ -7,7 +7,7 @@ class Unipag_Test_TestCase extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        Unipag_Config::$api_key = $this::SEC_KEY;
+        Unipag_Config::$api_key = self::SEC_KEY;
         Unipag_Config::$api_url = 'https://api.unipag.com/v1';
     }
 }
